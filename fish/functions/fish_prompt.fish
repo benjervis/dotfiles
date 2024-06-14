c10f8a60f1f181cacd32fb658ee301c1b66343cd
Ben Jervis <ben@jervis.net.au>
2024-06-14 16:38:46 +1000

    Don't render the git section of fish prompt if not a git repo

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,17 +1,26 @@
 function fish_prompt
-    string join '' -- \
+    set formatted_pwd (string join '' -- \
         (set_color --background green) \
         (set_color black) \
         " " \
         (prompt_pwd --full-length-dirs 2) \
         " " \
-        (set_color normal) \
-        " " \
+        (set_color normal))
+
+    set git_prompt_result (fish_git_prompt)
+
+    if test -n "$git_prompt_result"
+        set formatted_git (string join '' -- \
         (set_color --background yellow) \
         (set_color black) \
-        (fish_git_prompt) \
+        $git_prompt_result \
         ' ' \
-        (set_color normal)
+        (set_color normal))
+    else
+        set formatted_git ""
+    end
+
+    string join -n ' ' $formatted_pwd $formatted_git
     echo '> '
 end
 
