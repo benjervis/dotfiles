a52d53758bb5e1eaf54a41fee756c18c742552a7
Ben Jervis <ben@jervis.net.au>
2024-07-01 10:52:29 +1000

    Disable autopairs for single quotes in Rust that come after a <

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -18,3 +18,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  callback = function(args)
+    MiniPairs.map_buf(
+      args.buf,
+      "i",
+      "'",
+      { action = "closeopen", pair = "''", neigh_pattern = "[^%a<\\].", register = { cr = false } }
+    )
+  end,
+  desc = "Disable single quote autopairs in Rust",
+})
