7f199d136c483686d31ba8e54f336e50f1c26d8f
Ben Jervis <ben@jervis.net.au>
2024-07-08 21:08:02 +1000

    Setting some noice settings

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -219,4 +219,28 @@ return {
       },
     },
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    opts = {
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = true,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
 }
