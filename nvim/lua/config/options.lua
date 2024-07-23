3dc1e6fe2c7753b193f47b37ca30ed3402f4a061
Ben Jervis <ben@jervis.net.au>
2024-07-23 15:01:35 +1000

    Turn off most of rust-analyzer's inlay hints (except closing brace)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,30 @@ vim.g.lazyvim_picker = "fzf"
 
 vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
 
+vim.g.rustaceanvim = {
+  server = {
+    default_settings = {
+      ["rust-analyzer"] = {
+        inlayHints = {
+          bindingModeHints = { enable = false },
+          chainingHints = { enable = false },
+          closingBraceHints = { enable = true, minLines = 10 },
+          closureCaptureHints = { enable = false },
+          closureReturnTypeHints = { enable = "never" },
+          discriminantHints = { enable = "never" },
+          expressionAdjustmentHints = { enable = "never", hideOutsideUnsafe = true },
+          genericParameterHints = {
+            const = { enable = false },
+            lifetime = { enable = false },
+            type = { enable = false },
+          },
+          implicitDrops = { enable = false },
+          lifetimeElisionHints = { enable = "never" },
+          parameterHints = { enable = false },
+          rangeExclusiveHints = { enable = false },
+          typeHints = { enable = false, hideClosureInitialization = false, hideNamedConstructor = false },
+        },
+      },
+    },
+  },
+}
