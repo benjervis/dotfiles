b5b670908a94fbfbabc849c18ee234954ee4cd6b
Ben Jervis <ben@jervis.net.au>
2024-08-16 14:05:06 +1000

    Use which-key for keymaps directly

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,15 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local wk = require("which-key")
 
 local path_yank = require("config.path-yank")
 
-vim.keymap.set("n", "<leader>cv", ":lua=")
+wk.add({ "<leader>cy", path_yank.copy_path, mode = { "n", "v" }, desc = "Copy current path" })
+wk.add({ "<leader>cv", ":lua=", desc = "Lua Command" })
 
-vim.keymap.set({ "n", "v" }, "<leader>cy", path_yank.copy_path, { desc = "Copy current path" })
 
-vim.keymap.set("n", "<leader>gg", function()
-  LazyVim.lazygit({ size = { width = 1, height = 0.95 } })
-end, { desc = "Lazygit (Root Dir)" })
+wk.add({
+  "<leader>gg",
+  function()
+    LazyVim.lazygit({ size = { width = 1, height = 0.95 } })
+  end,
+  desc = "Lazygit (Root Dir)",
+})
